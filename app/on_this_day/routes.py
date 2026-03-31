f8e75b09a4b575e2e166cb4889ec1a90777adccb
Linh Pham <question+github@closedsrc.org>
2026-03-30 19:29:49 -0700

    Reworked validating of month and day for `app.on_this_day.routes.month_day()`

--- app/on_this_day/routes.py
+++ app/on_this_day/routes.py
@@ -29,6 +29,7 @@ def index() -> str:
     _details: dict[str, list[dict[str, Any]]] = retrieve_details(
         month=_date.month, day=_date.day, database_connection=_database_connection
     )
+    _database_connection.close()
 
     return render_template(
         "on_this_day/_report.html",
@@ -45,20 +46,23 @@ def month_day(show_month: int, show_day: int) -> str:
     if not current_app.config["app_settings"]["enable_on_this_day_report"]:
         return redirect_url(url_for("main.index"), status_code=302)
 
-    if not 1 <= show_month <= 12 or not 1 <= show_day <= 31:
+    try:
+        # Using 2004 as the year due to needing to a year that is a leap year.
+        # The year value is not used as part of the queries or when rendering
+        # the page.
+        _temp_date: date = date(year=2004, month=show_month, day=show_day)
+
+        _database_connection = mysql.connector.connect(**current_app.config["database"])
+        _details: dict[str, list[dict[str, Any]]] = retrieve_details(
+            month=show_month, day=show_day, database_connection=_database_connection
+        )
+        _database_connection.close()
+
+        return render_template(
+            "on_this_day/_report.html",
+            date=_temp_date,
+            details=_details,
+            specific_month_day=True,
+        )
+    except ValueError:
         return redirect_url(url_for("on_this_day.index"), status_code=302)
-
-    # Using 2004 as the year due to needing to a year that is a leap year. The
-    # year value is not used as part of the queries or when rendering the page.
-    _temp_date: date = date(year=2004, month=show_month, day=show_day)
-    _database_connection = mysql.connector.connect(**current_app.config["database"])
-    _details: dict[str, list[dict[str, Any]]] = retrieve_details(
-        month=show_month, day=show_day, database_connection=_database_connection
-    )
-
-    return render_template(
-        "on_this_day/_report.html",
-        date=_temp_date,
-        details=_details,
-        specific_month_day=True,
-    )
